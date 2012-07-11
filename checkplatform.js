453feb7bfbd37fbbdf57376f64de9c9e103223da
Tomasz Janczuk <tomasz@janczuk.org>
2012-07-11 16:39:05 -0700

    update pre-install step

--- checkplatform.js
+++ checkplatform.js
@@ -1,1 +1,8 @@
-require('./lib/tripwire.js');
+try {
+	require('./lib/tripwire.js');
+}
+catch (e) {
+	console.log('***************************************');
+	console.log(e);
+	console.log('***************************************');
+}
