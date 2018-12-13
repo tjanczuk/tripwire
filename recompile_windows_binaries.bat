549f793c9af44726b853eca7b2632be5e6ba3ebf
Eduardo <edunmc@gmail.com>
2018-12-13 13:52:10 -0200

    Updated Windows pre-compiled binaries, added bat script to autogenerate them.

---
+++ recompile_windows_binaries.bat
@@ -0,0 +1,29 @@
+mkdir "lib\native\win32\8\x64\"
+mkdir "lib\native\win32\9\x64\"
+mkdir "lib\native\win32\10\x64\"
+mkdir "lib\native\win32\11\x64\"
+
+mkdir "lib\native\win32\8\ia32\"
+mkdir "lib\native\win32\9\ia32\"
+mkdir "lib\native\win32\10\ia32\"
+mkdir "lib\native\win32\11\ia32\"
+
+call node-gyp clean configure build --arch=x64 --target=v8.14.0
+move /Y "build\Release\tripwire.node" "lib\native\win32\8\x64\"
+call node-gyp clean configure build --arch=ia32 --target=v8.14.0
+move /Y "build\Release\tripwire.node" "lib\native\win32\8\ia32\"
+
+call node-gyp clean configure build --arch=x64 --target=v9.11.2
+move /Y "build\Release\tripwire.node" "lib\native\win32\9\x64\"
+call node-gyp clean configure build --arch=ia32 --target=v9.11.2
+move /Y "build\Release\tripwire.node" "lib\native\win32\9\ia32\"
+
+call node-gyp clean configure build --arch=x64 --build_v8_with_gn=false --target=v10.14.2
+move /Y "build\Release\tripwire.node" "lib\native\win32\10\x64\"
+call node-gyp clean configure build --arch=ia32 --build_v8_with_gn=false --target=v10.14.2
+move /Y "build\Release\tripwire.node" "lib\native\win32\10\ia32\"
+
+call node-gyp clean configure build --arch=x64 --build_v8_with_gn=false --target=v11.4.0
+move /Y "build\Release\tripwire.node" "lib\native\win32\11\x64\"
+call node-gyp clean configure build --arch=ia32 --build_v8_with_gn=false --target=v11.4.0
+move /Y "build\Release\tripwire.node" "lib\native\win32\11\ia32\"
